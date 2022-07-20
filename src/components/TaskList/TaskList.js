143a04b222de9b1a237664ddb88971b677688e57
Анастасия <anastasiyakhokhlova22@gmail.com>
2022-07-20 14:58:41 +0300

    add handling errors function while changing status

--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import TaskItem from "./TaskItem/taskItem";
 import listStyle from "./styles.module.css";
 import filterStyle from "./filterstyle.module.css";
-import ClearAll from "../ClearAll/ClearAll"
+import ClearAll from "../ClearAll/ClearAll";
 import { ArrowUp } from "../../assets/arrows";
 import { ArrowDown } from "../../assets/arrows";
 import { LastPage } from "../../assets/pagesNavigationIcons";
@@ -10,18 +10,26 @@ import { FirstPage } from "../../assets/pagesNavigationIcons";
 import PaginationButtons from "./Pagination/PaginationButtons";
 import axios from "axios";
 
-const TaskList = ({ tasksCount, setTasksCount, todo, setTodo, currentPage, setCurrentPage, pagesAmount, setPagesAmount, MAX_NOTES }) => {
-
+const TaskList = ({
+  tasksCount,
+  setTasksCount,
+  todo,
+  setTodo,
+  currentPage,
+  setCurrentPage,
+  pagesAmount,
+  setPagesAmount,
+  MAX_NOTES,
+}) => {
   const filtersByState = {
     all: 10,
     done: 1,
     undone: 0,
-  }
+  };
 
-  const [inputValue, setInputValue] = useState('');
+  const [inputValue, setInputValue] = useState("");
   const [activeFilter, setActiveFilter] = useState(filtersByState.all);
   const [isAscendingSort, setAscendingSort] = useState(true);
-
 
   const dateSort = () => {
     setAscendingSort(!isAscendingSort);
@@ -32,33 +40,38 @@ const TaskList = ({ tasksCount, setTasksCount, todo, setTodo, currentPage, setCu
   };
 
   const deleteTodo = (uuid) => {
-    axios.delete(`https://todo-api-learning.herokuapp.com/v1/task/3/${uuid}`).then(() => {
-      const deletedTask = todo.filter((item) => item.uuid !== uuid);
-      setTodo(deletedTask);
-      setTasksCount(tasksCount - 1)
-    }).catch((e) =>alert(`Error! ${e}`))
-
-  }
+    axios
+      .delete(`https://todo-api-learning.herokuapp.com/v1/task/3/${uuid}`)
+      .then(() => {
+        const deletedTask = todo.filter((item) => item.uuid !== uuid);
+        setTodo(deletedTask);
+        setTasksCount(tasksCount - 1);
+      })
+      .catch((e) => alert(`Error! ${e}`));
+  };
 
   const changeTaskStatus = (uuid, done) => {
-    axios.patch(`https://todo-api-learning.herokuapp.com/v1/task/3/${uuid}`, {
-      done: !done
-    }).then(() => {
-      setTodo(
-        todo.filter((item) => {
-          if (item.uuid === uuid) {
-            item.done = !item.done;
-          }
-          return item;
-        })
-      );
-      if (activeFilter === 1) {
-        setTodo(todo.filter((item) => item.done));
-      }
-      if (activeFilter === 0) {
-        setTodo(todo.filter((item) => !item.done));
-      }
-    }).catch((e) =>console.log(`Error! ${e}`))
+    axios
+      .patch(`https://todo-api-learning.herokuapp.com/v1/task/3/${uuid}`, {
+        done: !done,
+      })
+      .then(() => {
+        setTodo(
+          todo.filter((item) => {
+            if (item.uuid === uuid) {
+              item.done = !item.done;
+            }
+            return item;
+          })
+        );
+        if (activeFilter === 1) {
+          setTodo(todo.filter((item) => item.done));
+        }
+        if (activeFilter === 0) {
+          setTodo(todo.filter((item) => !item.done));
+        }
+      })
+      .catch((error) => alert(`Oops! something went wrong! Status code: ${error.response.status}`))
   };
 
   const todoFilter = (status) => {
@@ -68,8 +81,8 @@ const TaskList = ({ tasksCount, setTasksCount, todo, setTodo, currentPage, setCu
     } else {
       let newTodo;
       if (status === filtersByState.done) {
-        newTodo = todo.filter((item) => item.done)
-        setTodo(newTodo)
+        newTodo = todo.filter((item) => item.done);
+        setTodo(newTodo);
       } else {
         newTodo = todo.filter((item) => !item.done);
         setTodo(newTodo);
@@ -85,18 +98,6 @@ const TaskList = ({ tasksCount, setTasksCount, todo, setTodo, currentPage, setCu
     setCurrentPage(pagesAmount);
   };
 
-  // useEffect(() => {
-  //   rerender ()
-  // }, [todo]);
-
-  // useEffect(() => {
-  //   setPagesAmount(countPages(filtered));
-  // }, [activeFilter, filtered]);
-
-  // useEffect(() => {
-  //   setFiltered(filtered);
-  // }, [filtered, currentPage, activeFilter]);
-
   useEffect(() => {
     setCurrentPage(1);
   }, [activeFilter]);
@@ -111,26 +112,25 @@ const TaskList = ({ tasksCount, setTasksCount, todo, setTodo, currentPage, setCu
 
   useEffect(() => {
     const fetchData = async () => {
-
-      let filter = '';
+      let filter = "";
       if (activeFilter === 1) {
-        filter = 'done'
+        filter = "done";
       }
       if (activeFilter === 0) {
-        filter = 'undone'
+        filter = "undone";
       }
 
-      const { data } = await axios
-        .get(
-          `https://todo-api-learning.herokuapp.com/v1/tasks/3?filterBy=${filter}&order=${isAscendingSort ? 'asc' : 'desc'}&pp=5&page=${currentPage}`
-        )
+      const { data } = await axios.get(
+        `https://todo-api-learning.herokuapp.com/v1/tasks/3?filterBy=${filter}&order=${
+          isAscendingSort ? "asc" : "desc"
+        }&pp=5&page=${currentPage}`
+      );
       setTodo(data.tasks);
-      setTasksCount(data.count)
-    }
-    fetchData()
-    fetchData().catch((e) =>alert(`error!${e}`))
-  }, [currentPage, activeFilter, isAscendingSort,tasksCount]) //including todo causes flashing
-
+      setTasksCount(data.count);
+    };
+    fetchData();
+    fetchData().catch((e) => alert(`error!${e}`));
+  }, [currentPage, activeFilter, isAscendingSort, tasksCount]); //including todo causes flashing
 
   return (
     <div>
@@ -169,15 +169,17 @@ const TaskList = ({ tasksCount, setTasksCount, todo, setTodo, currentPage, setCu
           <p>Sort by date</p>
           <div className={filterStyle.arrows}>
             <button className={filterStyle.arrows} onClick={dateSort}>
-              {isAscendingSort ? <ArrowUp arrowClass={filterStyle.arrow} /> : <ArrowDown arrowClass={filterStyle.arrow} />
-              }
+              {isAscendingSort ? (
+                <ArrowUp arrowClass={filterStyle.arrow} />
+              ) : (
+                <ArrowDown arrowClass={filterStyle.arrow} />
+              )}
             </button>
           </div>
         </div>
       </div>
       <ul className={listStyle.tasks}>
         {todo.map((item) => (
-
           <TaskItem
             key={item.uuid}
             todo={todo}
@@ -195,22 +197,25 @@ const TaskList = ({ tasksCount, setTasksCount, todo, setTodo, currentPage, setCu
           <FirstPage buttonClass={listStyle.pageNav} />
         </span>
         <div className={listStyle.pages}>
-          {
-            new Array(pagesAmount).fill().map((_, i) => (
-              <PaginationButtons
-                i={i + 1}
-                changePage={changePage}
-                currentPage={currentPage}
-                key={i}
-              />
-            ))
-          }
+          {new Array(pagesAmount).fill().map((_, i) => (
+            <PaginationButtons
+              i={i + 1}
+              changePage={changePage}
+              currentPage={currentPage}
+              key={i}
+            />
+          ))}
         </div>
         <span onClick={toTheLastPage}>
           <LastPage buttonClass={listStyle.pageNav} />
         </span>
       </div>
-      <ClearAll todo={todo} setTodo={setTodo} currentPage={currentPage} setCurrentPage={setCurrentPage} />
+      <ClearAll
+        todo={todo}
+        setTodo={setTodo}
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+      />
     </div>
   );
 };
