72b22a74d5da2354433345efc0e5d65ea53f18d2
Анастасия <anastasiyakhokhlova22@gmail.com>
2022-07-20 15:51:06 +0300

    Add handling of errors while delete all tasks from page

--- src/components/ClearAll/ClearAll.js
+++ src/components/ClearAll/ClearAll.js
@@ -1,24 +1,34 @@
 import React, { useState } from "react";
-import styles from "./clearall_btnstyle.module.css"
+import styles from "./clearall_btnstyle.module.css";
 import { ClearAllButtonIcon } from "../../assets/clearAllButtonIcon";
 import axios from "axios";
 
-function ClearAll({ todo, setTodo, setCurrentPage,currentPage }) {
+function ClearAll({ todo, setTodo, setCurrentPage, currentPage }) {
   const clearList = () => {
-    todo.forEach(element => {axios.delete(`https://todo-api-learning.herokuapp.com/v1/task/3/${element.uuid}`).then (()=>{
-      setTodo([]);
-      setCurrentPage(currentPage-1 || 1)
-    })});
+    todo.forEach((element) => {
+      axios
+        .delete(
+          `https://todo-api-learning.herokuapp.com/v1/task/3/${element.uuid}`
+        )
+        .then(() => {
+          setTodo([]);
+          setCurrentPage(currentPage - 1 || 1);
+        })
+        .catch((error) => {
+          switch (error.response.status) {
+            case 404:
+              alert('Task not found. It seems like the task has been already deleted');	
+              break;
+              default:
+                alert(`Oops! something went wrong! Status code: ${error.response.status}`)
+          }
+        })
+    });
   };
   return (
     <div>
-      <button
-        type="button"
-        className={styles.cleanbtn}
-        onClick={clearList}
-      >
-        Clear this page{" "}
-        <ClearAllButtonIcon clsName={styles.cleanbtn_icon} />
+      <button type="button" className={styles.cleanbtn} onClick={clearList}>
+        Clear this page <ClearAllButtonIcon clsName={styles.cleanbtn_icon} />
       </button>
     </div>
   );
